c8c5ed36e61c40aa86848512db2b16fc6b9a277d
Sira <siraphop_2538@hotmail.com>
2023-01-31 06:45:52 +1100

    Update html and css

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// class Evaluation {
+//   constructor(selectElement) {
+//     this.element = document.querySelector(selectElement);
+//   }
+// }
+// const nine = new Evaluation(".nine");
+
+// nine.element.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log("nine");
+// });
