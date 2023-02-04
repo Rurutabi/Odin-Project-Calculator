a90e2bbe4525bc81d395f03324373935b192713a
Sira <siraphop_2538@hotmail.com>
2023-02-04 17:54:15 +1100

    Add operator function

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 "use strict";
 
+const allButton = document.querySelectorAll(".btn");
+const before = document.querySelector(".before");
+const after = document.querySelector(".after");
+
 class Calculator {
-  constructor(beforeCalculation, afterCalculation) {
-    this.beforeCalculation = beforeCalculation;
-    this.afterCalculation = afterCalculation;
+  beforeCalculation = "";
+  operator;
+  afterCalculation;
+  constructor() {
+    // this.beforeCalculation = beforeCalculation;
+    // this.afterCalculation = afterCalculation;
+
+    this.whatButton();
+  }
+
+  whatButton() {
+    allButton.forEach((value) =>
+      value.addEventListener("click", (e) => {
+        this.beforeCalculation += e.target.innerText;
+
+        if (e.target.innerText === "+") {
+          console.log(e.target.innerText);
+        } else if (e.target.innerText === "-") {
+          console.log(e.target.innerText);
+        } else if (e.target.innerText === "x") {
+          console.log(e.target.innerText);
+        } else if (e.target.innerText === "÷") {
+          console.log(e.target.innerText);
+        } else {
+          console.log("something wrong");
+        }
+      })
+    );
+  }
+
+  checkOperator() {}
+
+  add(number1, number2) {
+    return number1 + number2;
+  }
+
+  subtract(number1, number2) {
+    return number1 - number2;
+  }
+
+  mutiply(number1, number2) {
+    return number1 * number2;
+  }
+
+  divide(number1, number2) {
+    return number1 / number2;
   }
 }
 
-const allButton = document.querySelectorAll(".btn");
-
-allButton.forEach((value) =>
-  value.addEventListener("click", (e) => {
-    if (e.target.innerText === "9") {
-      console.log("hi");
-    }
-  })
-);
+const justCal = new Calculator("", "");
